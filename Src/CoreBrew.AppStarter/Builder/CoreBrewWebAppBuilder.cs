8bd7f7d0a2cea96a8682ad1b7bec044b9a476953
Jeppe Clausen <jeppe712@gmail.com>
2023-05-19 23:29:16 +0200

    Start of CoreBrewWebAppBuilder

---
+++ Src/CoreBrew.AppStarter/Builder/CoreBrewWebAppBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace CoreBrew.AppStarter.Builder;
+
+public class CoreBrewWebAppBuilder : IApplicationBuilder
+{
+    private IApplicationBuilder _applicationBuilderImplementation;
+    public IApplicationBuilder Use(Func<RequestDelegate, RequestDelegate> middleware)
+    {
+        return _applicationBuilderImplementation.Use(middleware);
+    }
+
+    public IApplicationBuilder New()
+    {
+        return _applicationBuilderImplementation.New();
+    }
+
+    public RequestDelegate Build()
+    {
+        return _applicationBuilderImplementation.Build();
+    }
+
+    public IServiceProvider ApplicationServices
+    {
+        get => _applicationBuilderImplementation.ApplicationServices;
+        set => _applicationBuilderImplementation.ApplicationServices = value;
+    }
+
+    public IFeatureCollection ServerFeatures => _applicationBuilderImplementation.ServerFeatures;
+
+    public IDictionary<string, object?> Properties => _applicationBuilderImplementation.Properties;
+}
