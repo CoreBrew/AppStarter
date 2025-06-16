8e6f893e1137014b668199b1d621f37b4c957b73
jeppec <jeppe712@gmail.com>
2025-06-16 23:38:41 +0200

    Test commit

--- Samples/AppStarterWpfSample/App.xaml.cs
+++ Samples/AppStarterWpfSample/App.xaml.cs
@@ -9,6 +9,7 @@ public partial class App
 {
     protected override void OnStartup(StartupEventArgs e)
     {
+        //test
         Host = new AppBuilder().Build();
         base.OnStartup(e);
     }
